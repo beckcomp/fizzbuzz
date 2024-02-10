47b0d1ca26c65d6b6fc13f28cd9bccc31f987f00
Pedro Beck <pedro.beck@inf.ufrgs.br>
2024-02-09 21:35:03 -0300

    removed float

--- fizzbuzz6.c
+++ fizzbuzz6.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-#define c(x) *(int*)&x
+
 int main(void)
 {
-    float c = 7.31319893532e-10;
-    for (int i = 0; i ++< 100; c(c) = (c(c)>>2)|((c(c)&3)<<28)) {
+    int c = 810092048;
+    for (int i = 0; i ++< 100; c = (c>>2)|((c&3)<<28)) {
         char o[10] = "%d\n"; void *p = o;
-        if (c(c)&1) sprintf(p-4, "Fizz\n", p+=4);
-        if (c(c)&2) sprintf(p, "Buzz\n");
+        if (c&1) sprintf(p-4, "Fizz\n", p+=4);
+        if (c&2) sprintf(p, "Buzz\n");
         printf(o, i);
     }
     return 0;
